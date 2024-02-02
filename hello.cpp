b95b63bfe249c6b16f44c943ae944557bdd631e0
dannydawg <158222861+dannydawg@users.noreply.github.com>
2024-02-01 18:01:10 -0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {Hello World Lab 1}
+ ***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}

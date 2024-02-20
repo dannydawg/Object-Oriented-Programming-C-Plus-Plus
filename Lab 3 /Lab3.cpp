0e66fc4b3dd9ecc04e9df494c8eb1625725856da
dannydawg <158222861+dannydawg@users.noreply.github.com>
2024-02-19 23:05:29 -0800

    Lab 3

---
+++ Lab 3 /Lab3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+
+int x = 0;
+int y = 5;
+  
+  while(x<y){
+        printf("%i\n",x);
+        x++;
+    }
+    printf("Looped through 5 times\n");
+
+    if (x % 2 == 0){
+        printf("x is even");
+        }else{
+            printf("x is odd");
+        }    
+
+}
